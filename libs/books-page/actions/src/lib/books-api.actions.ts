639ccd4535f6e6f70f2cf67f8b6b232cd85b8416
johnrsimoneau <johnrsimoneau@gmail.com>
2022-04-30 06:54:02 -0400

    added book API actions

--- libs/books-page/actions/src/lib/books-api.actions.ts
+++ libs/books-page/actions/src/lib/books-api.actions.ts
@@ -1,4 +1,25 @@
 import { createAction, props } from '@ngrx/store';
-import { BookRequiredProps } from '@book-co/shared-models';
+import { BookModel } from '@book-co/shared-models';
 
+// NOTE you would also add a Failure action
+// not doing it in the demo to save time
+export const booksLoaded = createAction(
+  '[Books API] Books Loaded Success',
+  props<{ books: BookModel }>()
+);
+
+export const bookCreated = createAction(
+  '[Books API] Book Created Success',
+  props<{ book: BookModel }>()
+);
+
+export const bookUpdated = createAction(
+  '[Books API] Book Updated Success',
+  props<{ book: BookModel}>()
+);
+
+export const bookDeleted = createAction(
+  'Books API] Book Deleted Success',
+  props<{ bookId: string}>()
+);
  
