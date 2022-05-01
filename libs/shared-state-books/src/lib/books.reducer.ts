9e3e05a8da80cd91214709f21310110721c9ccb3
johnrsimoneau <johnrsimoneau@gmail.com>
2022-05-01 06:28:01 -0400

    Added selectors

--- libs/shared-state-books/src/lib/books.reducer.ts
+++ libs/shared-state-books/src/lib/books.reducer.ts
@@ -57,3 +57,45 @@ export const reducer = createReducer(
     }
   })
 )
+
+// Instructor likes to keeps selectors and reducers in the same file
+// thought is: there's a connection between the state - because they're
+// still related
+
+export const selectAll = (state: State) => state.collection;
+export const selectActiveBookId = (state: State) => state.activeBookId;
+
+/*
+export const selectActiveBook_bad_performance = (state: State) => {
+  const books = selectAll(state);
+  const activeBookId = selectActiveBookId(state);
+  return books.find(book => book.id === activeBookId) || null;
+};
+*/
+
+// the createSelector function gives better performance than the function above as it
+// it updates when any of the inputs change (i.e. the selectAll and/or selectActiveBookId)
+export const selectActiveBook = createSelector(
+  selectAll,
+  selectActiveBookId,
+  // below is a callback function - projector function
+  // first param is the selectAll, second param is the selectedBookId
+  (books, activeBookId) => {
+    return books.find(book => book.id === activeBookId) || null;
+  }
+);
+
+/*
+
+Below is the long hand way, we're creating the projector function by hand.
+We can do below this greyed out example - which is a more succinct way to write
+the same thing
+
+export const selectEarningsTotals = createSelector(selectAll, (books) => {
+  return calculateBooksGrossEarnings(books);
+});
+*/
+export const selectEarningsTotals = createSelector(
+  selectAll,
+  calculateBooksGrossEarnings
+);
--- libs/shared-state-books/src/lib/state.ts
+++ libs/shared-state-books/src/lib/state.ts
@@ -39,3 +39,30 @@ export const selectSharedBooksState = createFeatureSelector<State>(FEATURE_KEY);
 /**
  * Books Selectors
  */
+export const selectBooksState = createSelector(
+  selectSharedBooksState,
+  (sharedBooksFeatureState) => sharedBooksFeatureState.books
+);
+/*
+long-hand way:
+export const selectAllBooks = createSelector(
+  selectBooksState,
+  (booksState) => fromBooks.selectAll(booksState)
+)
+
+Because the signature of this projector function (booksState) => fromBooks.selectAll(booksState)
+matches the signature of the signature of fromBooks.selectAll(booksState) - you can write it like below
+*/
+export const selectAllBooks = createSelector(
+  selectBooksState,
+  fromBooks.selectAll
+);
+export const selectActiveBook = createSelector(
+  selectBooksState,
+  fromBooks.selectActiveBook
+);
+export const selectBooksEarningsTotals = createSelector(
+  selectBooksState,
+  fromBooks.selectEarningsTotals
+)
+
